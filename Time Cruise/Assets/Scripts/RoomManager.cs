61983b7540c6c1c8d54e0a1da38081146aaa7b86
Théo <theo.querment96@gmail.com>
2018-11-17 17:42:22 -0400

    room et mapmanager

---
+++ Time Cruise/Assets/Scripts/RoomManager.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        setRenderer(transform, true);
+        //foreach (Transform child in transform)
+        //{
+        //    Renderer renderer = child.GetComponent<Renderer>();
+        //    if (renderer)
+        //    {
+        //        renderer.enabled = true;
+        //    }
+        //    else
+        //    {
+        //        foreach (Transform littleChild in child.transform)
+        //        {
+        //            littleChild.GetComponent<Renderer>().enabled = true;
+        //        }
+        //    }
+
+        //}
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        setRenderer(transform, false);
+        //foreach (Transform child in transform)
+        //{
+        //    Renderer renderer = child.GetComponent<Renderer>();
+        //    if (renderer)
+        //    {
+        //        renderer.enabled = false;
+        //    }
+        //    else
+        //    {
+        //        foreach (Transform littleChild in child.transform)
+        //        {
+        //            littleChild.GetComponent<Renderer>().enabled = false;
+        //        }
+        //    }
+
+        //}
+    }
+
+
+
+    void setRenderer(Transform obj, bool visible)
+    {
+        foreach (Transform child in obj)
+        {
+            Renderer renderer = child.GetComponent<Renderer>();
+            if (renderer)
+            {
+                renderer.enabled = visible;
+            }
+            else
+            {
+                setRenderer(child.transform, visible);
+            }
+
+        }
+    }
+
+}
