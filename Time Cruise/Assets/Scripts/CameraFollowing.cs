62fa1a398149f6b3367883dde0d871dfb3975b61
Théo <theo.querment96@gmail.com>
2018-11-17 12:53:20 -0400

    stairs

---
+++ Time Cruise/Assets/Scripts/CameraFollowing.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CameraFollowing : MonoBehaviour
+{
+
+    public GameObject followed;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 v = followed.transform.position;
+        v.z = -2;
+        transform.position = v;
+
+    }
+}
