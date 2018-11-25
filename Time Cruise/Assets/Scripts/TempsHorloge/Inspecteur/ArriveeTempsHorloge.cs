ff2a849b72617b9764bd279e1cf75efe82fc2fa2
Unknown <objecris@gmail.com>
2018-11-25 18:47:07 -0400

    Début du niveau

---
+++ Time Cruise/Assets/Scripts/TempsHorloge/Inspecteur/ArriveeTempsHorloge.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ArriveeTempsHorloge : MonoBehaviour
+{
+    public DialogueTrigger inspecteur;
+
+    public UnityEvent OnArriveeEnd;
+
+    void Start()
+    {
+        StartCoroutine("Reflection");
+    }
+
+    private IEnumerator Reflection()
+    {
+        inspecteur.SetSentence("*Que s’est il passé?*");
+        yield return new WaitForSeconds(2);
+        inspecteur.ResetSentence();
+
+        inspecteur.SetSentence("*Le capitaine est encore en vie ! Et l’horloge fonctionne…*");
+        yield return new WaitForSeconds(2);
+        inspecteur.ResetSentence();
+
+        OnArriveeEnd.Invoke();
+    }
+}
