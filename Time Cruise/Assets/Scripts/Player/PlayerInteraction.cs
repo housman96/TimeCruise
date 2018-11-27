63fc111fab3f82666a880d59749853f4bb5e3987
Unknown <objecris@gmail.com>
2018-11-27 15:22:18 -0400

    Presque fini

--- Time Cruise/Assets/Scripts/Player/PlayerInteraction.cs
+++ Time Cruise/Assets/Scripts/Player/PlayerInteraction.cs
@@ -41,7 +41,6 @@ public class PlayerInteraction : MonoBehaviour {
         {
             if (rayHits[x].collider != null)    // S'il y'a collision
             {
-                Debug.Log(rayHits[x].transform.gameObject.name);
                 Interactable interactible = rayHits[x].transform.GetComponent<Interactable>();
                 availableInteractables[x] = interactible;
             }
