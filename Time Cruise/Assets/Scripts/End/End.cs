389760c14e03123eff3fa0c5904b4702d1cb94a7
housman <theo.querment96@gmail.com>
2018-11-30 00:58:56 -0900

    Debut de la fin

---
+++ Time Cruise/Assets/Scripts/End/End.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class End : MonoBehaviour
+{
+
+    public GameObject inspecteur;
+    public GameObject capitaine;
+    public GameObject pierre;
+    public GameObject tapis;
+    public GameObject key;
+    public DialogueTrigger capitaineDial;
+    public DialogueTrigger pierreDial;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(end());
+    }
+
+    public IEnumerator end()
+    {
+        yield return new WaitForSeconds(1);
+
+        /*CACHER SOUS LE TAPIS*/
+
+        GameObject targetObjectCapitaine = new GameObject();
+        targetObjectCapitaine.transform.position = new Vector3(-0.24f, 2.1f);
+        capitaine.GetComponent<Pathfinding.AIDestinationSetter>().target = targetObjectCapitaine.transform;
+
+        yield return new WaitForSeconds(1);
+
+        tapis.SetActive(false);
+        yield return new WaitForSeconds(1);
+        key.SetActive(true);
+        yield return new WaitForSeconds(1);
+        key.SetActive(false);
+        tapis.SetActive(true);
+
+        capitaineDial.SetSentence("Et voilà personne n'ira la chercher ici.");
+        yield return new WaitForSeconds(5);
+        capitaineDial.ResetSentence();
+
+        capitaineDial.SetSentence("Ho inspecteur, je ne vous avez pas vu.");
+        yield return new WaitForSeconds(5);
+        capitaineDial.ResetSentence();
+
+        capitaineDial.SetSentence("Pouvez vous sortir s'il-vous-plait? Mon fils ne devrait pas tarder.");
+        yield return new WaitForSeconds(5);
+        capitaineDial.ResetSentence();
+
+        GameObject targetObjectinspecteur = new GameObject();
+        targetObjectinspecteur.transform.position = new Vector3(-14.5f, 5.4f);
+        inspecteur.GetComponent<Pathfinding.AIDestinationSetter>().target = targetObjectinspecteur.transform;
+
+        GameObject targetObjectPierre = new GameObject();
+        targetObjectPierre.transform.position = new Vector3(-1f, 2.1f);
+        pierre.GetComponent<Pathfinding.AIDestinationSetter>().target = targetObjectPierre.transform;
+
+        yield return new WaitForSeconds(3);
+        pierreDial.SetSentence("Bonjour Inspecteur.", false);
+        yield return new WaitForSeconds(3);
+        pierreDial.ResetSentence();
+
+    }
+
+}
--- Time Cruise/Assets/Scripts/Present/Accusation.cs
+++ Time Cruise/Assets/Scripts/Present/Accusation.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Accusation : MonoBehaviour
 {
@@ -7,7 +8,10 @@ public class Accusation : MonoBehaviour
     public GameObject inspecteur;
     public GameObject matelot1;
     public GameObject matelot2;
+    public GameObject longueVue;
     public DialogueTrigger inspecteurDial;
+    public DialogueTrigger pierreDial;
+
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -31,6 +35,8 @@ public class Accusation : MonoBehaviour
         targetObjectInspecteur.transform.position = new Vector3(-6.42f, -14.14f);
         inspecteur.GetComponent<Pathfinding.AIDestinationSetter>().target = targetObjectInspecteur.transform;
 
+        yield return new WaitForSeconds(1);
+
         inspecteurDial.SetSentence("Messieurs veuillez me suivre.", false);
         yield return new WaitForSeconds(4);
         inspecteurDial.ResetSentence();
@@ -48,7 +54,32 @@ public class Accusation : MonoBehaviour
         matelot1.GetComponent<Pathfinding.AIDestinationSetter>().target = targetObjectMatelot1.transform;
         matelot2.GetComponent<Pathfinding.AIDestinationSetter>().target = targetObjectMatelot2.transform;
 
+        /*ACCUSATION*/
 
+        yield return new WaitForSeconds(10);
+
+        inspecteurDial.SetSentence("Pierre, au vu des indices que j'ai pu récolter, je vous accuse du meurtre de votre père.", false);
+        yield return new WaitForSeconds(4);
+        inspecteurDial.ResetSentence();
+
+        pierreDial.SetSentence("Non, vous faites erreur je vous assure.", false);
+        yield return new WaitForSeconds(4);
+        pierreDial.ResetSentence();
+
+        inspecteurDial.SetSentence("Messieurs, veuillez fouiller Pierre s'il-vous-plait.", false);
+        yield return new WaitForSeconds(4);
+        inspecteurDial.ResetSentence();
+
+        longueVue.SetActive(true);
+        yield return new WaitForSeconds(2);
+
+        inspecteurDial.SetSentence("La longue vue du Capitaine, surement l'arme du crime.", false);
+        yield return new WaitForSeconds(4);
+        inspecteurDial.ResetSentence();
+
+        targetObjectInspecteur.transform.position = new Vector3(20.60f, -22.59f);
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadSceneAsync(1);
     }
 
 
