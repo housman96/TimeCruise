5e0339897fd4dc4896157ea55a728a26e7b827b5
jb <jb751996@hotmail.fr>
2018-12-02 18:50:18 -0500

    Menu pause et son

---
+++ Time Cruise/Assets/Menu/Scripts/SettingsVolume.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsVolume : MonoBehaviour {
+
+    public AudioMixer audioMixer;
+
+    public void SetMasterVolume(float volume) {
+        audioMixer.SetFloat("masterVolume",volume);
+    }
+
+    public void SetMusicVolume(float volume) {
+        audioMixer.SetFloat("musicVolume", volume);
+    }
+
+    public void SetEffectsVolume(float volume) {
+        audioMixer.SetFloat("effectsVolume", volume);
+    }
+}
