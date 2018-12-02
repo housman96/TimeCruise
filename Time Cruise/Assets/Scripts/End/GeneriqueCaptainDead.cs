476dd7414bf4de3e5e2af70da09e077d2e6c52f1
housman <theo.querment96@gmail.com>
2018-12-01 20:52:12 -0900

    Mise en place

---
+++ Time Cruise/Assets/Scripts/End/GeneriqueCaptainDead.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GeneriqueCaptainDead : MonoBehaviour
+{
+    public GameObject blackScreen;
+    public GameObject capitaine;
+    //public GameObject pierre;
+    //public GameObject marie;
+    //public GameObject luigi;
+    //public GameObject julie;
+    //public GameObject jean;
+    //public GameObject isabelle;
+    //public GameObject paul;
+    //public GameObject detective;
+    public Text text;
+
+    public void launchGenerique()
+    {
+        StartCoroutine(generique());
+    }
+
+    public IEnumerator generique()
+    {
+        yield return new WaitForSeconds(1);
+
+
+        float alpha = 0;
+        float alphaText = 0;
+        //float alphaPerso = 0;
+
+        while (alpha < 1)
+        {
+            yield return new WaitForSeconds(Time.deltaTime);
+            alpha += 0.02f;
+            blackScreen.GetComponent<Image>().color = new Color(0, 0, 0, alpha);
+        }
+        blackScreen.GetComponent<Image>().color = new Color(0, 0, 0, 1);
+
+
+        /*CAPITAINE*/
+        text.text = "Le capitaine n'a pas survecu à ses blessures.";
+
+        while (alphaText < 1)
+        {
+            yield return new WaitForSeconds(Time.deltaTime);
+            alphaText += 0.02f;
+            text.GetComponent<Text>().color = new Color(1, 1, 1, alphaText);
+            capitaine.GetComponent<Image>().color = new Color(1, 1, 1, alphaText);
+        }
+        text.GetComponent<Text>().color = new Color(1, 1, 1, 1);
+        capitaine.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+
+    }
+}
