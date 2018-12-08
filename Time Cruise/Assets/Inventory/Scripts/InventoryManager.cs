07afca9de7ddfc0c9691a92b6536c727a122fe4a
jb <jb751996@hotmail.fr>
2018-12-08 17:19:21 -0500

    Correction fermeture inventaire

--- Time Cruise/Assets/Inventory/Scripts/InventoryManager.cs
+++ Time Cruise/Assets/Inventory/Scripts/InventoryManager.cs
@@ -7,16 +7,21 @@ public class InventoryManager : MonoBehaviour {
     private PlayerController playerController;
     private FurnitureInventory activeInventory;
     public GameObject InventoryUI;
-    private KeyCode closeInventory=KeyCode.A;
+    //private KeyCode closeInventory=KeyCode.E;
 
     private void Start() {
         playerController = FindObjectOfType<PlayerController>();
     }
     public void StartInventory(FurnitureInventory inventory) {
-        activeInventory = inventory;
-        playerController.LockMoves();
-        activeInventory.loadInventory(InventoryUI);
-        ShowInventory();
+        if (!InventoryUI.activeInHierarchy) {
+            activeInventory = inventory;
+            playerController.LockMoves();
+            activeInventory.loadInventory(InventoryUI);
+            ShowInventory();
+        }
+        else {
+            EndInventory();
+        }
     }
     public void ShowInventory() {
         InventoryUI.SetActive(true);
@@ -31,9 +36,11 @@ public class InventoryManager : MonoBehaviour {
     public void onSlotClicked (int slot) {
         activeInventory.onSlotClicked(slot,InventoryUI);
     }
-    private void Update() {
-        if (Input.GetKeyDown(closeInventory)) {
+    /*private void Update() {
+        if (Input.GetKeyDown(closeInventory) && lastTimeOpen+delayBeforeClose<Time.time) {
+            Debug.Log("close");
             EndInventory();
+            lastTimeClosed = Time.time;
         }
-    }
+    }*/
 }
