76deccb81dab70a5f7223ab78655a312d15d88d1
housman96 <32963399+housman96@users.noreply.github.com>
2021-03-20 17:04:00 -0400

    Fix exponential volume settings

--- Time Cruise/Assets/Menu/Scripts/SettingsVolume.cs
+++ Time Cruise/Assets/Menu/Scripts/SettingsVolume.cs
@@ -2,20 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsVolume : MonoBehaviour {
-
+    public Slider sliderVolumeMaster;
+    public Slider sliderVolumeMusic;
+    public Slider sliderVolumeEffects;
     public AudioMixer audioMixer;
 
     public void SetMasterVolume(float volume) {
+        if (volume <= sliderVolumeMaster.minValue)
+        {
+            volume = -80;
+        }
         audioMixer.SetFloat("masterVolume",volume);
     }
 
     public void SetMusicVolume(float volume) {
+        if (volume <= sliderVolumeMaster.minValue)
+        {
+            volume = -80;
+        }
         audioMixer.SetFloat("musicVolume", volume);
     }
 
     public void SetEffectsVolume(float volume) {
+        if( volume <= sliderVolumeMaster.minValue)
+        {
+            volume = -80;
+        }
         audioMixer.SetFloat("effectsVolume", volume);
     }
 }
